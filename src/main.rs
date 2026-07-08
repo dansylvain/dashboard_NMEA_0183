8a2f6e22e770515b48dd63cab1f5d90dc2d3595e
Dan SYLVAIN <dansylvain84@gmail.com>
2026-07-08 16:01:04 +0200

    implemented TerminalGuard

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use anyhow::{Context, Result};
+use crossterm::execute;
+use crossterm::terminal::{
+    EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode,
+};
+use std::io;
+
+struct TerminalGuard {}
+
+impl TerminalGuard {
+    fn new() -> Result<Self> {
+        enable_raw_mode().context("failed to enable raw mode")?;
+        execute!(io::stdout(), EnterAlternateScreen).context("Failed to enter alternate screen")?;
+        Ok(Self {})
+    }
+}
+
+impl Drop for TerminalGuard {
+    fn drop(&mut self) {
+        if let Err(e) = execute!(io::stdout(), LeaveAlternateScreen)
+            .context("Failed to exit alternate screen: ")
+        {
+            eprintln!("{}", e);
+        }
+        if let Err(e) = disable_raw_mode().context("failed to disable raw mode: ") {
+            eprintln!("{}", e);
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, Dashboard NMEA!");
 }
